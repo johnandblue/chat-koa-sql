3e15499e5089a26fd2cc2829f5f0952c0bea9ae9
johnandblue <jc@johnandblue.com>
2016-12-01 19:06:39 +0100

    mongoose ok

--- models/messages.js
+++ models/messages.js
@@ -16,47 +16,22 @@ Message.message = mongoose.Schema({
 
 Message.Msgmodel = mongoose.model('Messages', Message.message);
 
-Message.Msgmodel.create({user: 'user1', content: 'Hola feo', timestamp: 'thursday'}, function (err, message) {
-  if (err) return handleError(err);
-})
-
-Message.Msgmodel.create({user: 'forismatic', content: 'Hola guapo', timestamp: 'thursday'}, function (err, message) {
-  if (err) return handleError(err);
-})
-
 Message.getAllMessages = function () {
-  // return new Promise(function (resolve, reject) {
-  //   Message.Msgmodel.find(function (err, messages){
-  //     if (err) return console.log(err);
-  //     console.log(messages);
-  //     resolve(messages);
-  //   })
-  // });
   return Message.Msgmodel.find();
 };
 
 Message.postMessage = function(body) {
-
+  return Message.Msgmodel.create({user: body.user, content: body.content, timestamp: body.timestamp}, function (err, message) {
+    // if (err) return handleError(err);
+    console.log(body);
+  })
 }
-
-
-// Message.getAllMessages = function () {
-//   return new Promise(function (resolve, reject) {
-//     db.query('SELECT* from messages', function (err, rows, fields) {
-//       if (err) reject(err);
-//       console.log(rows);
-//       resolve(rows);
-//     });
-//   });
-// };
 //
-// Message.postMessage = function(body) {
-//   db.query(" INSERT into messages (user, content, timestamp) VALUES ('"+body.user+" ',' "+body.content+" ',' "+body.timestamp+"');" );
-// }
-//
-// Message.deleteMsgs = function() {
-//   db.query("truncate messages");
-// }
+Message.deleteMsgs = function() {
+  Message.Msgmodel.remove({}, function(){
+    console.log('deleted');
+  });
+}
 
 
 module.exports = Message;
